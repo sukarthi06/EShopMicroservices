2fa1ff0b3b04590f3473a4b27d9b212e91844b00
Karthikeya Sundaramoorthy <sukarthilenovo@outlook.com>
2026-01-26 13:36:08 -0500

    Added category based product filter

--- client/eshop-app/src/features/products/product-list/product-list.ts
+++ client/eshop-app/src/features/products/product-list/product-list.ts
@@ -9,26 +9,41 @@ import { Product } from '../../../types/product';
   styleUrl: './product-list.css',
 })
 export class ProductList implements OnInit {
-  
+    
   protected products = signal<Product[]>([]);
+  private allProducts = signal<Product[]>([]);
   protected productService = inject(ProductService);
+  protected categories: string[] = [];
 
   protected readonly lastProduct = computed(() => {
     const items = this.products();
     return items.length ? items[items.length - 1] : undefined;
   });
 
-  protected readonly categories = computed(() =>
-    [...new Set(this.products().flatMap(p => p.category))]
-  );
-
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
       next: response => {        
         this.products.set(response.products);
+        this.allProducts.set(response.products);
+        this.setCategories();
       },
       error: error => console.log(error)
     });
+  }
+
+  setCategories(): void {
+    const uniqueCategories = this.products()
+      .flatMap(p => p.category)
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    this.categories = ['All', ...uniqueCategories];
+  }
+
+  filterProductsByCategory(category: string): void {
+    this.products.set(this.allProducts());
+    if (category === 'All') return;
+    const filteredProducts = this.products().filter(p => p.category.includes(category));
+    this.products.set(filteredProducts);
   }
 
 }
