187c2acf1e5714a4177b95d534d76512ae670f73
Karthikeya Sundaramoorthy <sukarthilenovo@outlook.com>
2026-01-26 22:17:29 -0500

    Added product detail page

--- client/eshop-app/src/app/app.routes.ts
+++ client/eshop-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductList } from '../features/products/product-list/product-list';
 import { Home } from '../features/home/home';
+import { ProductDetail } from '../features/products/product-detail/product-detail';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'products', component: ProductList }
+    { path: 'products', component: ProductList },
+    { path: 'productdetail/:id', component: ProductDetail }
 ];
--- client/eshop-app/src/core/services/product-service.ts
+++ client/eshop-app/src/core/services/product-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Product, ProductsResponse } from '../../types/product';
+import { Product, ProductResponse, ProductsResponse } from '../../types/product';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +13,9 @@ export class ProductService {
 
   getProducts(){
     return this.http.get<ProductsResponse>(this.baseUrl + 'products');
+  }
+
+  getProductById(id: string){
+    return this.http.get<ProductResponse>(this.baseUrl + 'products/' + id);
   }
 }
